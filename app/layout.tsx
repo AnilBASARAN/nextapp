a8d365da2599c6870f63be4ef9284033a7501b24
Anıl Başaran <nlbsrn@gmail.com>
2024-09-06 15:59:14 +0300

    added a navigation bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
 import ProductCard from './component/ProductCard'
+import NavigationBar from './component/navigation/page'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,9 +21,18 @@ export default function RootLayout({
   return (
    
     <html data-theme="winter" lang="en">
+      
 
            
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <NavigationBar />
+        <main>
+        {children}
+        </main>
+      
+        
+
+      </body>
     </html>
   )
 }
